5339d2b783e31bc9c42ea90f63639a30ac6a9a13
mlee277 <mlee277@gmail.com>
2015-12-09 15:51:42 -0800

    initial commit

---
+++ javascript.js
@@ -0,0 +1,67 @@
+var skylink = new Skylink();
+
+skylink.on('peerJoined', function(peerId, peerInfo, isSelf) {
+  var user = 'You';
+  if(!isSelf) {
+    user = peerInfo.userData.name || peerId;
+  }
+  addMessage(user + ' joined the room', 'action');
+});
+
+skylink.on('peerUpdated', function(peerId, peerInfo, isSelf) {
+  if(isSelf) {
+    user = peerInfo.userData.name || peerId;
+    addMessage('You\'re now known as ' + user, 'action');
+  }
+});
+
+skylink.on('peerLeft', function(peerId, peerInfo, isSelf) {
+  var user = 'You';
+  if(!isSelf) {
+    user = peerInfo.userData.name || peerId;
+  }
+  addMessage(user + ' left the room', 'action');
+});
+
+skylink.on('incomingMessage', function(message, peerId, peerInfo, isSelf) {
+  var user = 'You',
+      className = 'you';
+  if(!isSelf) {
+    user = peerInfo.userData.name || peerId;
+    className = 'message';
+  }
+  addMessage(user + ': ' + message.content, className);
+});
+
+skylink.init('62ab516d-76cc-49ae-931d-d31507ec07c5');
+
+
+function setName() {
+  var input = document.getElementById('name');
+  skylink.setUserData({
+    name: input.value
+  });
+}
+
+function joinRoom() {
+  skylink.joinRoom();
+}
+
+function leaveRoom() {
+  skylink.leaveRoom();
+}
+
+function sendMessage() {
+  var input = document.getElementById('message');
+  skylink.sendP2PMessage(input.value);
+  input.value = '';
+  input.select();
+}
+
+function addMessage(message, className) {
+  var chatbox = document.getElementById('chatbox'),
+    div = document.createElement('div');
+  div.className = className;
+  div.textContent = message;
+  chatbox.appendChild(div);
+}
